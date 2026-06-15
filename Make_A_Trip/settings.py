c2056d7e0530a6cdecc0cb87de4eb82d3acb6231
Zsa Aupo <zakariasarker407@gmail.com>
2026-06-16 04:25:08 +0600

    2 app added

--- Make_A_Trip/settings.py
+++ Make_A_Trip/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.user',
+    'app.hotel',
 ]
 
 MIDDLEWARE = [
@@ -72,10 +74,21 @@ WSGI_APPLICATION = 'Make_A_Trip.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'make_a_trip',
+        'USER': 'root',
+        'PASSWORD': '927599',
+        'HOST': 'localhost', 
+        'PORT': '3306',
     }
 }
 
